ac5f053af47c9f69b9a795019449dc5840b2ee5d
shanon cymon <shanonsimon082@gmail.com>
2025-01-15 20:43:52 +0300

    Initial commit: Employee management system

--- index.php
+++ index.php
@@ -4,12 +4,54 @@ include("header.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Employees</title>
+    <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
-    <h1>welcome </h1>
+    <div class="container">
+        <h1>Employees</h1>
+        <div class="buttons">
+            <div class="create-btn">
+                <button type="submit">Add Employee</button>
+            </div>
+            <div class="other-btns">
+                <button>filter</button>
+                <button>sort</button>
+                <input type="text" placeholder="search employee">
+            </div>
+        </div>
+        <div class="em-table">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Other names</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                    <th>More info</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                </tr>
+                <tr>
+                    <td>001</td>
+                    <td>kato</td>
+                    <td>Deo</td>
+                    <td></td>
+                    <td>Male</td>
+                    <td>30</td>
+                    <td><a href="#">more details</a></td>
+                    <td><a href="#">Update employee</a></td>
+                    <td><a href="#">Delete employee</a></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </body>
+
 </html>
